5268b447d84bf91a5d3f2bf3859ec52dfd65d367
Armin Schreger <armin@schreger.at>
2025-01-10 08:06:10 +0100

    Rm index on tmux window creation
    Could be in conflict with existing index

--- extend_tmux.sh
+++ extend_tmux.sh
@@ -5,9 +5,9 @@ set -eu
 cd $(git rev-parse --show-toplevel)
 
 # Create tailwind tab
-tmux new-window -t taranis:3 -n tailwind 
+tmux new-window -t taranis -n tailwind 
 tmux send-keys -t taranis:tailwind "./tailwindcss -i scheduler/static/css/input.css -o scheduler/static/css/tailwind.css --watch" C-m
 
 # Create scheduler tab
-tmux new-window -t taranis:4 -n scheduler 
+tmux new-window -t taranis -n scheduler 
 tmux send-keys -t taranis:scheduler "./install_and_run_dev.sh" C-m
